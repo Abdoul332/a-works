5ff5b48dd7af9ad059b7d52e7603f879e8645a3b
Abdoul332 <abdoulibrahim332@gmail.com>
2025-05-03 19:00:20 +0200

    gamepad

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,16 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./.html', './/.js'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    safelist: ['show', 'collapse', /^modal/, /^navbar/, /^dropdown/]
-  });
-  
-  module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-    ]
-  };
+const purgecss = require('@fullhuman/postcss-purgecss').default;
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    purgecss({
+      content: [
+        './*.html',
+        './**/*.html',
+        './**/*.js'
+      ],
+      safelist: [/^navbar/, /^btn/, /^card/, /^col/, /^row/, /^container/],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+}
